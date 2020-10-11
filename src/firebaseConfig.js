684db42a10c4c6a72eb6bc532c003b1fa88e9c9f
hypermystic <72652958+hypermystic@users.noreply.github.com>
2020-10-11 19:14:36 +0530

    Done some fixes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,14 @@ import '@firebase/firestore';
 import 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuLCITyz_Lcls05znU8-JYEsZh9gZeOaQ",
-  authDomain: "bookit-8abd4.firebaseapp.com",
-  databaseURL: "https://bookit-8abd4.firebaseio.com",
-  projectId: "bookit-8abd4",
-  storageBucket: "bookit-8abd4.appspot.com",
-  messagingSenderId: "52238525209",
-  appId: "1:52238525209:web:facab65e0b980fc14ddb88",
-  measurementId: "G-5Y4EVCSFRW"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
